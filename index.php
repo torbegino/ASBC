d0012562f299bdf4180b44981aa2c58ec548d702
Antonio Rodriguez Martinez <antonio@nobox.com>
2013-09-27 22:42:01 -0400

    Reverted back the files.

--- index.php
+++ index.php
@@ -4,12 +4,5 @@
 </head>
 <body>
 	<h1>ASBC</h1>
-	<p>Estas brutal.</p>
-	
-	<h2>Si, esto esta brutal, me encanta</h2>
-	<p>Otro parrafo. Testing.</p>
-	
-	<p>Wow, que cool. Testing.</p>
-	<h3>Hacer, lo que tu quieras.</h3>
 </body>
 </html>
